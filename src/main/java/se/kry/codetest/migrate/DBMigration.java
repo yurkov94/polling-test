317ede946f93e3fc631de3d7adb2758d7f61777e
alex <yurkovx@gmail.com>
2020-10-12 02:32:21 +0100

    - updated initial schema for service table

--- src/main/java/se/kry/codetest/migrate/DBMigration.java
+++ src/main/java/se/kry/codetest/migrate/DBMigration.java
@@ -8,7 +8,7 @@ public class DBMigration {
   public static void main(String[] args) {
     Vertx vertx = Vertx.vertx();
     DBConnector connector = new DBConnector(vertx);
-    connector.query("CREATE TABLE IF NOT EXISTS service (url VARCHAR(128) NOT NULL)").setHandler(done -> {
+    connector.query("CREATE TABLE IF NOT EXISTS service (id INTEGER NOT NULL PRIMARY KEY, url VARCHAR(128) NOT NULL, name VARCHAR(128) NOT NULL UNIQUE)").setHandler(done -> {
       if(done.succeeded()){
         System.out.println("completed db migrations");
       } else {
